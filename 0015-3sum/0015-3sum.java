fab7680e1d5950d43e9ec1572f009d308da10372
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-05-27 23:32:10 +0530

    Time: 29 ms (78.32%), Space: 53.1 MB (11.44%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        int n = nums.length;
-        Arrays.sort(nums);
-        TreeSet<String> set = new TreeSet<String>();
-        for(int i=0;i<n-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) {            
-                continue;
-            }
-            int left = i+1;
-            int right = n-1;
-            
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum == 0){
-                    String str = nums[i]+":"+nums[left]+":"+nums[right];
-                    if(!set.contains(str)){
-                        res.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                        set.add(str);
+    Arrays.sort(nums);
+        LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            //Only consider non-duplicate elements for i
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        ArrayList<Integer> miniSol = new ArrayList<>();
+                        miniSol.add(nums[i]);
+                        miniSol.add(nums[left]);
+                        miniSol.add(nums[right]);
+                        sol.add(miniSol);
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] > target) {
+                        right--;
+                    } else {
+                        left++;
                     }
-                    left++;
-                    right--;
-                }
-                else if(sum > 0){
-                    right--;
-                }
-                 else if(sum < 0){
-                    left++;
                 }
             }
         }
-        return res;
+
+        return sol;
     }
 }
