1e9b1aa65c9e3a120c129388440ca228df0d62ae
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-08-01 14:10:55 +0530

    Time: 1 ms (82.64%), Space: 41.9 MB (97.63%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -3,7 +3,7 @@ class Solution {
         int SeniorCitizen = 0;
         
         for(String s : details){
-            if(s.charAt(11) > '6' || s.charAt(11) >='6' && s.charAt(12) > '0') SeniorCitizen++;
+            if(s.charAt(11) > '6' || s.charAt(11) =='6' && s.charAt(12) > '0') SeniorCitizen++;
         }
         
         return SeniorCitizen;
