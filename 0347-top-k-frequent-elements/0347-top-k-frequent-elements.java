d4e3de1b717004d38b77442aa415be82c875a5a0
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-09-12 20:55:24 +0530

    Time: 14 ms (56.17%), Space: 45.3 MB (96.00%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        List<Integer>[] freq = new List[nums.length + 1];
-
-        for (int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
+       Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        for (int n : nums) {
-            count.put(n, count.getOrDefault(n, 0) + 1);
-        }
+        // Step 2: Create a min heap (PriorityQueue) based on frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
+            (a, b) -> a.getValue() - b.getValue()  // Min-heap based on frequency
+        );
 
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            freq[entry.getValue()].add(entry.getKey());
+        // Step 3: Add elements to the heap, and maintain the top K frequent elements
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            minHeap.add(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();  // Remove the least frequent element
+            }
         }
 
         int[] res = new int[k];
-        int index = 0;
-        for (int i = freq.length - 1; i > 0 && index < k; i--) {
-            for (int n : freq[i]) {
-                res[index++] = n;
-                if (index == k) {
-                    return res;
-                }
-            }
+
+        for(int i=0;i<k;i++){
+            res[i] = minHeap.poll().getKey();
         }
+
         return res;
     }
 }
