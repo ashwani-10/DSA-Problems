c16e338d24ff12cd38a67ff73ba7e50701288b96
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-07 16:23:59 +0530

    Time: 8 ms (25.26%), Space: 45.4 MB (76.66%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,34 +1,37 @@
 class Solution {
-    public static int dfs(int r, int c, int[][] grid, int[][] vis) {
-    if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) {
-        return 1;
+   public static void dfs(int r,int c,int[][] grid,int[][] vis,int []perimeter){
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c] == 0){
+            perimeter[0]++;
+            return;
+        }
+
+        if(vis[r][c] == 1)
+        return;
+
+        vis[r][c] = 1;
+
+        dfs(r+1,c,grid,vis,perimeter);
+        dfs(r-1,c,grid,vis,perimeter);
+        dfs(r,c+1,grid,vis,perimeter);
+        dfs(r,c-1,grid,vis,perimeter);
+
+
     }
-    if (vis[r][c] == 1) {
-        return 0;
-    }
-
-    vis[r][c] = 1;
-
-    int perim = 0;
-    perim += dfs(r+1, c, grid, vis); // check down neighbor
-    perim += dfs(r-1, c, grid, vis); // check up neighbor
-    perim += dfs(r, c+1, grid, vis); // check right neighbor
-    perim += dfs(r, c-1, grid, vis); // check left neighbor
-
-    return perim;
-}
 
     public static int islandPerimeter(int[][] grid) {
-    int r = grid.length;
-    int c = grid[0].length;
-    int [][] vis = new int[r][c];
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            if(grid[i][j] == 1){
-                return dfs(i, j, grid, vis);
+        // your code here
+        int r = grid.length;
+        int c = grid[0].length;
+        int [][] vis = new int[r][c];
+        int []count =new int[1];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 1){
+                    dfs(i,j,grid,vis,count);
+                    return count[0];
+                }
             }
         }
+        return -1;
     }
-    return -1;
-}
 }
