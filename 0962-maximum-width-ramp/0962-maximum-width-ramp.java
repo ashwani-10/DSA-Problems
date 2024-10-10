aa3d59a4e3e0069759a8bc94104c58d609a4364e
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-10 15:55:42 +0530

    Time: 4 ms (84.83%), Space: 54.7 MB (40.55%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        
+        int [] maxArr = new int[n];
+        maxArr[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            maxArr[i] = Math.max(nums[i],maxArr[i+1]);
+        }
+        
+        int l=0,r=0,width=0;
+        
+        while(r<n){
+            if(maxArr[r] >= nums[l])
+                width = Math.max(width,r-l);
+            
+            while(nums[l] > maxArr[r])
+                l++;
+            
+            r++;
+            
+        }
+        return width;
+    }
+}
