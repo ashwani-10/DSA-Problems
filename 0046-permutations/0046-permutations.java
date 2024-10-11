a650dbce34c691128139e1ed23f7903ca072c12b
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-12 01:00:17 +0530

    Time: 1 ms (94.96%), Space: 44.4 MB (50.41%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void swap(int i,int j, int[]arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+
+    public void helper(int[]arr, int n, int idx, List<List<Integer>> result){
+        if(idx == n){
+            List<Integer> temp = new ArrayList();
+            for(int num : arr){
+                temp.add(num);
+            }
+            result.add(temp);
+            return;
+        }
+
+        for(int i=idx;i<n;i++){
+            swap(i,idx,arr);
+            helper(arr,n,idx+1,result);
+            swap(i,idx,arr);
+        }
+    }
+    public List<List<Integer>> permute(int[] arr) {
+        int n = arr.length;
+        List<List<Integer>> result = new ArrayList();
+        Arrays.sort(arr);
+        
+        helper(arr,n,0,result);
+        
+        return result;
+    }
+}
