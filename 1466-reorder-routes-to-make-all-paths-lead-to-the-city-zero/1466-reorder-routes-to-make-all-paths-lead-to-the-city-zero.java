4f5549c05f81b4812fcc862d1e385d7b4dcd5826
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-14 00:36:56 +0530

    Time: 44 ms (60.32%), Space: 73.4 MB (49.71%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,40 +1,20 @@
 class Solution {
-    public void dfs(int n,int node, int[][] connections,ArrayList<ArrayList<Integer>> forward,ArrayList<ArrayList<Integer>> backward,boolean[] vis,int[] ans){
-        if(n==0) return;
-        vis[node] = true;
-        
-        for(int num : forward.get(node)){
-            if(vis[num] == false)
-                ans[0]++;
-                dfs(n,num,connections,forward,backward,vis,ans);
-        }
-        
-        for(int num : backward.get(node)){
-            if(vis[num] == false)
-                dfs(n,num,connections,forward,backward,vis,ans);
-        }
-        
+    int dfs(List<List<Integer>> al, boolean[] visited, int from) {
+    int change = 0;
+    visited[from] = true;
+    for (var to : al.get(from))
+        if (!visited[Math.abs(to)])
+            change += dfs(al, visited, Math.abs(to)) + (to > 0 ? 1 : 0);
+    return change;   
+}
+public int minReorder(int n, int[][] connections) {
+    List<List<Integer>> al = new ArrayList<>();
+    for(int i = 0; i < n; ++i) 
+        al.add(new ArrayList<>());
+    for (var c : connections) {
+        al.get(c[0]).add(c[1]);
+        al.get(c[1]).add(-c[0]);
     }
-    public int minReorder(int n, int[][] connections) {
-        ArrayList<ArrayList<Integer>> forward = new ArrayList();
-        ArrayList<ArrayList<Integer>> backward = new ArrayList();
-        boolean[] vis = new boolean[n];
-        int[] ans = new int[1];
-         for(int i=0;i<n;i++){
-             forward.add(new ArrayList<Integer>());
-             backward.add(new ArrayList<Integer>());
-        }
-        
-        for(int i=0;i<n-1;i++){
-            int u = connections[i][0];
-            int v = connections[i][1];
-            
-            forward.get(u).add(v);
-            backward.get(v).add(u);
-        }
-        
-        dfs(n,0,connections,forward,backward,vis,ans);
-        
-        return ans[0];
-    }
+    return dfs(al, new boolean[n], 0);
+}
 }
