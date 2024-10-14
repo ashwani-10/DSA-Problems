0890731a6022ff6ddef0161185ef6b455d46df39
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-14 18:50:45 +0530

    Time: 24 ms (12.73%), Space: 49.9 MB (48.47%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,48 +1,15 @@
-class comp implements Comparator<Integer> {
-    public int compare(Integer a , Integer b) {
-        if(a > b)
-            return 1;
-        if(a < b)
-            return -1;
-        return 0;
-    }
-}
 class KthLargest {
-    int K;
-    PriorityQueue <Integer> pq;
-    public KthLargest(int k, int[] nums) {
-        K = k;
-        pq = new PriorityQueue <Integer> (new comp());
-        for(int x : nums) {
-            pq.add(x);
-            if(pq.size() > k) {
-                pq.remove();
-            }
-        }
-    }
-    int add(int val) {
-        pq.add(val);
-        if(pq.size() > K)
-            pq.remove();
-        return pq.peek();
-    }
-}
-class KthLargestInStream {
-    public static void main(String args[]) {
-        int k = 3;
-        int[] nums = {4 , 5 , 8 , 2};
-        KthLargest stream = new KthLargest(k , nums);
-        System.out.print(stream.add(3) + " ");
-        System.out.print(stream.add(5) + " ");
-        System.out.print(stream.add(10) + " ");
-        System.out.print(stream.add(9) + " ");
-        System.out.print(stream.add(4) + " ");
-        System.out.println();
-    }
-}
+  private PriorityQueue<Integer> heap = new PriorityQueue<>();
+  private int k;
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
+  public KthLargest(int k, int[] nums) {
+    this.k = k;
+    for (int n : nums) add(n);
+  }
+  
+  public int add(int val) {
+    heap.offer(val);
+    if (heap.size() > k) heap.poll();
+    return heap.peek();
+  }
+}
