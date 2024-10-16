9d68c179fe07299f1692a6baaa364eb22c03bcd5
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-16 23:55:20 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (30.47%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findPivot(int [] nums,int n){
+        int l = 0;
+        int r = n-1;
+
+        while(l<r){
+            int mid = l+(r-l)/2;
+
+            if(nums[mid] > nums[r]) l = mid+1;
+
+            else r=mid;
+        }
+        return r;
+    }
+
+    public int binarySearch(int l,int r,int [] nums,int target){
+        while(l<=r){
+            int mid = l+(r-l)/2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] > target) r=mid-1;
+
+            else l = mid+1;
+        }
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int pivot = findPivot(nums,n);
+
+        int idx = binarySearch(0,pivot-1,nums,target);
+
+        if(idx != -1)
+        return idx;
+
+        return binarySearch(pivot,n-1,nums,target);
+    }
+}
