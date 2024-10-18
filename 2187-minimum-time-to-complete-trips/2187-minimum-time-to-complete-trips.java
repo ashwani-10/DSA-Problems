779222a87398af0f12054e55b5b59e76dd22bc78
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-19 01:07:30 +0530

    Time: 47 ms (78.68%), Space: 57.7 MB (73.14%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -11,17 +11,17 @@ class Solution {
     public long minimumTime(int[] time, int totalTrips) {
         int n = time.length;
         long l = 1;
-        long r = Long.MIN_VALUE;
+        long r = (long) Integer.MAX_VALUE;
         for(int t : time){
-            r = Math.max(r,(long)t);
+            r = Math.min(r,(long)t);
         }
         r = (long) r*totalTrips;
         
-        while(l<=r){
+        while(l<r){
             long mid = l+(r-l)/2;
             
             if(check(time,mid,totalTrips)){
-                r=mid-1;
+                r=mid;
             }
             else{
                 l=mid+1;
