5cd0a13a93b3a4c2e42b4ef3c142dc53aed75698
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-10-26 01:37:51 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (69.29%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,11 +15,18 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) 
-        return true;
-        if((root1 == null && root2 != null) || (root2 == null && root1 != null) || root1.val != root2.val )
-        return false;
-
-        return isSameTree(root1.left,root2.left) && isSameTree(root1.right,root2.right);
+        if(root1 == null && root2 == null)
+            return true;
+        
+        if(root1 == null || root2 == null)
+            return false;
+        
+        if(root1.val != root2.val)
+            return false;
+        
+        boolean l = isSameTree(root1.left,root2.left);
+        boolean r = isSameTree(root1.right,root2.right);
+        
+        return l & r;
     }
 }
