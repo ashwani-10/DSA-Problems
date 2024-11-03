85f30014419a5c89c89765a7e08615d87c37f7a3
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-11-04 01:27:50 +0530

    Time: 1 ms (66.16%), Space: 45.3 MB (13.19%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -20,8 +20,21 @@ class Solution {
 
         for(int i=0;i<graph.length;i++){
             if(color[i] == -1){
-                if(!dfs(graph,i,color,1))
-                return false;
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                color[i] = 1;
+                
+                while(!q.isEmpty()){
+                    int node = q.remove();
+                    
+                    for(int it : graph[node]){
+                        if(color[it] == color[node]) return false;
+                        else if(color[it] == -1){
+                            color[it] = 1-color[node];
+                            q.add(it);
+                        }
+                    }
+                }
             }
         }
         return true;
