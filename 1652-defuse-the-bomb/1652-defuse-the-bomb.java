4a8db153f9b023e1539f79a932faa3c6fe3308b4
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-11-18 13:37:07 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (55.38%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        if(k == 0) return new int[code.length];
+        
+        int[] prefix = new int[code.length * 2 + 1];
+        for(int i = 0; i < code.length * 2; i++) {
+            prefix[i+1] = prefix[i] + code[i % code.length];
+        }
+        
+        int[] ret = new int[code.length];
+        for(int i = 0; i < code.length; i++) {
+            int end = i + k;
+            if(k < 0) {
+                ret[i] = prefix[i + code.length] - prefix[end + code.length];
+            }
+            else {
+                ret[i] = prefix[end + 1] - prefix[i+1];
+            }
+        }
+        
+        return ret;
+    }
+}
