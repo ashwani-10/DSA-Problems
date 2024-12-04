08ec729e63f733141c6adcc9a8c5c13a3ce55e11
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-12-04 23:18:24 +0530

    Time: 81 ms (5.29%), Space: 45.8 MB (14.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,33 +1,35 @@
 class Solution {
-    public void dfs(char [][] board,int i,int j){
+    public void dfs(char [][] board,int i,int j,int vis[][]){
         if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != 'O') return;
         
         board[i][j] = 'F';
+        vis[i][j] = 1;
         
-        dfs(board,i+1,j);
-        dfs(board,i-1,j);
-        dfs(board,i,j+1);
-        dfs(board,i,j-1);
+        dfs(board,i+1,j,vis);
+        dfs(board,i-1,j,vis);
+        dfs(board,i,j+1,vis);
+        dfs(board,i,j-1,vis);
     }
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
+        int vis[][] = new int[n][m];
         
         for(int j=0;j<m;j++){
             if(board[0][j] == 'O'){
-                dfs(board,0,j);
+                dfs(board,0,j,vis);
             }
             if(board[n-1][j] == 'O'){
-                dfs(board,n-1,j);
+                dfs(board,n-1,j,vis);
             }
         }
         
         for(int i=0;i<n;i++){
             if(board[i][0] == 'O'){
-                dfs(board,i,0);
+                dfs(board,i,0,vis);
             }
             if(board[i][m-1] == 'O'){
-                dfs(board,i,m-1);
+                dfs(board,i,m-1,vis);
             }
         }
         
