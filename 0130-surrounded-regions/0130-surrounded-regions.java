f382c3c7dc9537b8125017c05834053ffebc939a
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-12-04 23:21:21 +0530

    Time: 2 ms (84.92%), Space: 46 MB (9.79%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -42,11 +42,5 @@ class Solution {
             }
         }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
     }
 }
