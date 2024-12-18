559bad79cd6aaa0715650e34e5cf27ac16de16e8
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-12-18 22:35:58 +0530

    Time: 1 ms (98.90%), Space: 43.6 MB (60.92%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = new int[n];
+    
+        for(int i=0;i<n-1;i++){
+                boolean flag = false;
+            for(int j=i+1;j<n;j++){
+                    if(prices[j] <= prices[i]){
+                       flag = true;
+                       ans[i] = prices[i] - prices[j];
+                            break;
+                    }
+            }
+             if(flag == false){
+                ans[i] = prices[i]; 
+             }
+        }
+            ans[n-1] = prices[n-1];
+            return ans;
+    }
+}
