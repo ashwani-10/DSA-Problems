83667c152e9e9fb6b5b109b3cf53eb7e7c7f4a15
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-12-26 23:42:13 +0530

    Time: 1 ms (36.59%), Space: 42 MB (10.24%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int matrixScore(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i = 0; i < m; i++) {
+            if(grid[i][0] == 0) {
+                for(int j = 0; j < n; j++) {
+                    grid[i][j] = 1 - grid[i][j];
+                }
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            
+            int zero = 0;
+
+            for(int i = 0; i < m; i++) {
+                if(grid[i][j] == 0) zero++;
+            }
+
+            if(zero > m / 2) {
+                for(int i = 0; i < m; i++) {
+                    grid[i][j] = 1 - grid[i][j];
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < m; i++) {
+            int temp = 0;
+            for(int j = n-1; j >=0; j--) {
+                if(grid[i][j] == 1){
+                        ans += Math.pow(2,temp);
+                }
+                    temp++;
+            }
+        }
+
+        return ans;
+    }
+}
