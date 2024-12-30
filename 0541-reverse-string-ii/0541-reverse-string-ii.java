d57229679bc85e99241731129c4b1eaf98c8e267
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2024-12-30 16:16:32 +0530

    Time: 1 ms (72.05%), Space: 43.5 MB (48%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -6,8 +6,10 @@ class Solution {
             int i = start; int j = Math.min(start+k-1,str.length-1);
             while(i<j){
             char temp = str[i];
-            str[i++] = str[j];
-            str[j--] = temp;
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
         }
     }
     return new String(str);
