8a9de623ea16dba5c78a105414534bc357daa133
ashwani-10 <85514113+ashwani-10@users.noreply.github.com>
2025-01-03 00:19:54 +0530

    Time: 421 ms (5.08%), Space: 56.8 MB (78.74%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.java
@@ -5,10 +5,6 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
 
          for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                map.put(arr[i],map.get(arr[i])+1);
-            }
-            else
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
          }
 
