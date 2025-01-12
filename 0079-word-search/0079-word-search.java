da0b824a5d78ee5c190466fec7f709c3f37d0a5d
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-01-12 15:01:02 +0530

    Time: 203 ms (24.01%), Space: 41.5 MB (82.07%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,25 +1,25 @@
 class Solution {
 
     int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
-    public boolean helper(char board[][],String word,int i,int j,int idx){
-        if(i<0 || i>= board.length || j<0 || j>=board[0].length || word.charAt(idx) != board[i][j] || board[i][j] == '#'){
-            return false;
-        }
-        
-        if(idx == word.length()-1){
+    public boolean helper(char board[][],String word,boolean[][] vis, int i,int j,int idx){
+        if(idx == word.length()){
             return true;
         }
+       
+        if(i<0 || i>= board.length || j<0 || j>=board[0].length || word.charAt(idx) != board[i][j] || vis[i][j]){
+            return false;
+        }
 
-        char cur_char = board[i][j];
-        board[i][j] = '#';
+        vis[i][j] = true;
 
         for(int[] dr : dir){
-            if(helper(board,word,i+dr[0],j+dr[1],idx+1)){
+            if(helper(board,word,vis,i+dr[0],j+dr[1],idx+1)){
                 return true;
             }
         }
 
-        board[i][j] = cur_char;
+        vis[i][j] = false;
+
 
         return false;
 
@@ -28,10 +28,11 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
+        boolean[][] vis = new boolean[n][m];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                    if(board[i][j] == word.charAt(0) && helper(board,word,i,j,0)){
+                    if(board[i][j] == word.charAt(0) && helper(board,word,vis,i,j,0)){
                         return true;
                 }
             }
