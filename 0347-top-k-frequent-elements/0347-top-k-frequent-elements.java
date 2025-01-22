d560f98ae65a5e46c7a0f65dcd160d6b25713b81
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-01-22 19:17:58 +0530

    Time: 14 ms (56.56%), Space: 49.7 MB (6.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,42 @@
+class Pair{
+    int key;
+    int frq;
+
+    Pair(int key,int frq){
+        this.key = key;
+        this.frq = frq;
+    }
+}
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        int n = nums.length;
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        // Step 2: Create a min heap (PriorityQueue) based on frequency
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
-            (a, b) -> a.getValue() - b.getValue()  // Min-heap based on frequency
-        );
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1,p2) -> (p1.frq-p2.frq));
 
-        // Step 3: Add elements to the heap, and maintain the top K frequent elements
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            minHeap.add(entry);
-            if (minHeap.size() > k) {
-                minHeap.poll();  // Remove the least frequent element
+        for(Integer ky : map.keySet()){
+            int key = ky;
+            int frq = map.get(ky);
+
+            pq.add(new Pair(key,frq));
+
+            if(pq.size() > k){
+                pq.remove();
             }
         }
 
-        int[] res = new int[k];
-
-        for(int i=0;i<k;i++){
-            res[i] = minHeap.poll().getKey();
+        int[] ans = new int[k];
+        int i=0;
+        while(!pq.isEmpty()){
+            Pair p = pq.remove();
+            ans[i] = p.key;
+            i++;
         }
-
-        return res;
+        return ans;
     }
 }
