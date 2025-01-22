43ec9c621aa11e6f97f340794d924b32a4a62721
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-01-22 14:39:39 +0530

    Time: 10 ms (69.45%), Space: 85 MB (31.33%) - LeetHub

--- 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
@@ -7,31 +7,20 @@ class Solution {
         int[] col1 = new int[m];
 
         for(int i=0;i<n;i++){
-            int c1 = 0;
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1)
-                c1++;
+                if(grid[i][j] == 1){
+                    row1[i]++;
+                    col1[j]++;
+                }
             }
-            row1[i] = c1;
-        }
-
-        for(int i=0;i<m;i++){
-            int c1 = 0;
-            for(int j=0;j<n;j++){
-                if(grid[j][i] == 1)
-                c1++;
-            }
-            col1[i] = c1;
         }
 
         int[][] result = new int[n][m];
 
         for(int i=0;i<n;i++){
-            int rc1 = row1[i];
             for(int j=0;j<m;j++){
-                int cc1 = col1[j];
-                int value = rc1+cc1 - ((m-rc1)+(n-cc1));
-                result[i][j]=value;
+                
+                result[i][j]=row1[i]+col1[j] - ((m-row1[i])+(n-col1[j]));
             }
         }
 
