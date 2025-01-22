bd8c7d690dac66bff24831aad624f6333d76fab8
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-01-22 14:20:17 +0530

    Time: 13 ms (84.74%), Space: 47.3 MB (62.56%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,46 +1,40 @@
 class Solution {
-    int dir[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-    
-    public boolean isSafe(int nx, int ny, int n, int m){
-        return (nx>=0 && nx<n && ny>=0 && ny<m);
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    public boolean isSafe(int nx,int ny,int n,int m){
+        return nx >= 0 && nx<n && ny>=0 && ny<m;
     }
-    
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int [][] result = new int[n][m];
-        
-        Queue<int []> q = new LinkedList<>();
-        
+
+        int[][] result = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
-                    q.add(new int[] {i,j});
-                }
-                else{
-                    result[i][j] = Integer.MAX_VALUE;
+                    q.add(new int[]{i,j});
+                }else{
+                    result[i][j] = -1;
                 }
             }
         }
-        
+
         while(!q.isEmpty()){
-            int idx[] = q.poll();
-            int i = idx[0];
-            int j = idx[1];
-            
-            for(int d[] : dir){
+            int[] cords = q.remove();
+            int i = cords[0];
+            int j = cords[1];
+
+            for(int[] d : dir){
                 int nx = i+d[0];
                 int ny = j+d[1];
-                
-                if(isSafe(nx,ny,n,m) && result[nx][ny] > result[i][j] + 1){
+
+                if(isSafe(nx,ny,n,m) && result[nx][ny] == -1){
                     result[nx][ny] = result[i][j]+1;
-                    q.add(new int[] {nx,ny});
-                }
-                
+                    q.add(new int[]{nx,ny});
+                } 
             }
         }
-        
-        return result;
-        
+        return result; 
     }
 }
