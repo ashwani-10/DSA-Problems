1bd16a3fc764650e4c5988fef965279a38a046f2
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-01-24 15:28:08 +0530

    Time: 0 ms (100%), Space: 41.6 MB (98.28%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+
+        return helper(x,n);
+    }
+
+    public double helper(double x, int n) {
+        // Base case: anything raised to 0 is 1
+        if (n == 0) return 1;
+        
+        double half = helper(x, n / 2);
+        
+        // If n is even
+        if (n % 2 == 0) {
+            return half * half;
+        } else {  // If n is odd
+            return half * half * x;
+        }
+    }
+}
