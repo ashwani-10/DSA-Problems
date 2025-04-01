3ce1f1e560ab7a2e9b9a7785adf74ae855c0db5a
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-04-01 16:31:02 +0530

    Time: 1 ms (87.1%), Space: 41.3 MB (85.72%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,39 +1,27 @@
 class Solution {
-    int t[] = new int[366];
+    int dp[] = new int[366];
     public int solve(int[] days, int[] costs,int n,int i){
         if(i>=n)
         return 0;
 
-        if(t[i] != -1) return t[i];
-        //1 day pass
-        int cost_1 = costs[0] + solve(days,costs,n,i+1);
+        if(dp[i] != -1) return dp[i];
 
-        //7 day pass
-        int maxDays = days[i] + 7;
-        int j = i;
+        int cost_1 = costs[0] + solve(days,costs,n,newIndex(i,days,days[i]+1));
+        int cost_7 = costs[1] + solve(days,costs,n,newIndex(i,days,days[i]+7));
+        int cost_30 = costs[2] + solve(days,costs,n,newIndex(i,days,days[i]+30));
 
-        while(j < n && days[j] < maxDays){
-            j++;
+        return dp[i] = Math.min(Math.min(cost_1,cost_7),cost_30);
+
+    }
+    public int newIndex(int curr,int days[],int targetDay){
+        while(curr<days.length && days[curr] < targetDay){
+            curr++;
         }
-
-        int cost_7 = costs[1] + solve(days,costs,n,j);
-
-        //30 day pass
-        maxDays = days[i] + 30;
-        j = i;
-
-        while(j < n && days[j] < maxDays){
-            j++;
-        }
-
-        int cost_30 = costs[2] + solve(days,costs,n,j);
-
-        return t[i] = Math.min(Math.min(cost_1,cost_7),cost_30);
-
+        return curr;
     }
     public int mincostTickets(int[] days, int[] costs) {
         int n = days.length;
-        Arrays.fill(t,-1);
+        Arrays.fill(dp,-1);
         return solve(days,costs,n,0);
     }
 }
