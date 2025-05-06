e3bba34b21a8663004474f30a5a56408cc313300
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-05-06 16:46:43 +0530

    Time: 32 ms (44.84%), Space: 51.9 MB (37.3%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,15 +1,16 @@
 class Solution {
     List<List<Integer>> res;
-
-    public void twoSum(int i,int j,int target,int[] nums){
+    public void twoSum(int i,int j,int nums[],int target){
         while(i<j){
             int sum = nums[i]+nums[j];
             if(sum == target){
-                while(i<j && nums[j] == nums[j-1]) j--;
                 res.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],-target)));
+                while(i<j && nums[i] == nums[i+1]) i++;
+                while(i<j && nums[j] == nums[j-1]) j--;
                 i++;
                 j--;
-            }else if(sum > target){
+            }
+            else if(sum > target){
                 j--;
             }else{
                 i++;
@@ -17,14 +18,14 @@ class Solution {
         }
     }
     public List<List<Integer>> threeSum(int[] nums) {
-        res = new ArrayList<>();
         int n = nums.length;
+        res = new ArrayList<>();
         Arrays.sort(nums);
 
         for(int i=0;i<n;i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
             int target = -nums[i];
-            twoSum(i+1,n-1,target,nums);
+            twoSum(i+1,n-1,nums,target);
         }
         return res;
     }
