0d1f67a72f1f5ef68287f21f3a10c330706cf2f4
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-05-07 16:04:57 +0530

    Time: 4 ms (21.42%), Space: 47.6 MB (66.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,39 @@
 class Solution {
-    public void dfs(int node,int[][] isConnected,int[] vis){
+    public void dfs(int[] vis,int node,List<List<Integer>> adj){
         vis[node] = 1;
-        
-        for(int i=0;i<isConnected.length;i++){
-            if(isConnected[node][i] == 1 && vis[i] == 0){
-                dfs(i,isConnected,vis);
-            } 
+
+        for(int nb : adj.get(node)){
+            if(vis[nb] == 0){
+                dfs(vis,nb,adj);
+            }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int vis[] = new int[n];
-        int count = 0;
-        
+        List<List<Integer>> adj = new ArrayList<>();
+        int province = 0;
+
         for(int i=0;i<n;i++){
-            if(vis[i] == 0){
-                count++;
-                Queue<Integer> q = new LinkedList<>();
-                q.add(i);
-                
-                while(!q.isEmpty()){
-                    int node = q.remove();
-                    
-                    for(int k=0;k<isConnected.length;k++){
-                        if(isConnected[node][k] == 1 && vis[k] == 0){
-                            vis[k] = 1;
-                            q.add(k);
-                        } 
-                    }
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j] == 1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-        return count;
+        int[] vis = new int[n];
+
+        for(int i=0;i<n;i++){
+            if(vis[i] == 0){
+                province++;
+                dfs(vis,i,adj);
+            }    
+        }
+
+        return province;
     }
 }
