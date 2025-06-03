2718bb2850797b180b37d4cfaf1ecbb2fbfc884a
Ashwani <85514113+ashwani-10@users.noreply.github.com>
2025-06-03 21:55:56 +0530

    Time: 0 ms (100%), Space: 40.6 MB (39.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,9 @@
 class Solution {
-    int dp[];
+    int[] dp;
     public int helper(int n){
-        if(n == 0 || n == 1) return 1;
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+
         if(dp[n] != -1) return dp[n];
 
         return dp[n] = helper(n-1) + helper(n-2);
@@ -9,7 +11,7 @@ class Solution {
     public int climbStairs(int n) {
         dp = new int[n+1];
         Arrays.fill(dp,-1);
-        int maxVal = helper(n);
-        return maxVal;
+
+        return helper(n);
     }
 }
